0d97a8a8f440ce5b116ae87284a07f8f805e4350
Lucas Sousa <ls442336@gmail.com>
2022-02-01 20:21:37 -0300

    set traffics for genetic params experiment

--- tests/run_genetic_for_diff_params.sh
+++ tests/run_genetic_for_diff_params.sh
@@ -28,16 +28,16 @@ trap ctrlc INT
 # and 50% between different Pods.
 # "random" means choosing the iperf server randomly.
 # Change it if needed.
-traffics="stag_0.2_0.3"
+traffics="stag1_0.2_0.3 stag2_0.2_0.3 stag1_0.4_0.3 stag2_0.4_0.3 stag1_0.6_0.2 stag2_0.6_0.2 stag1_0.7_0.2 stag2_0.7_0.2"
 #traffics="stag_0.2_0.3 stag_0.3_0.3 stag_0.4_0.3 stag_0.5_0.3 stag_0.6_0.2 stag_0.7_0.2 stag_0.8_0.1"
 # Output directory.
-out_dir="./genetico_params_results_${k}"
+out_dir="./genetico_results/result_${k}"
 sudo rm -f -r out_dir 
 sudo mkdir -p $out_dir
 
-num_elements="0"
-geracoes=(10 100 100 50 50 25 25)
-populacao=(10 25 10 50 25 50 25)
+num_elements="0 1 2 3 4 5 6"
+geracoes=(100 100 100 50 50 25 25)
+populacao=(100 25 10 50 25 50 25)
 crossover=(0.6 0.6 0.6 0.6 0.6 0.6 0.6)
 mutacao=(0.25 0.25 0.25 0.25 0.25 0.25 0.25)
 
@@ -69,7 +69,7 @@ do
     NUM_TRAFFICS=$(expr $NUM_TRAFFICS + $NUM)
 
     # Create iperf peers.
-    sudo python ./../../exp_EFattree/create_peers.py --k $k --traffic $traffic --fnum $flows_num_per_host
+    sudo python ./create_peers.py --k $k --traffic $traffic --fnum $flows_num_per_host
     sleep 1
 
     sed -i "s/generations=.*[^,]/generations=${geracoes}/g" "./${target}/network_monitor.py"
@@ -86,3 +86,5 @@ do
   done
 
 done
+
+echo $(date)
