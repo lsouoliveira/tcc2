e2402afcb0ef76e60a7a5b2105b0fe8415b4edb5
Lucas Sousa <ls442336@gmail.com>
2022-02-23 18:36:40 -0300

    remove folders

--- tests/genetico_bak/config.py
+++
@@ -1,6 +0,0 @@
-k = 4
-
-class CONF:
-    fanout = k
-    weight = 'bw'
-    k_paths = k ** 2 * 3 / 4
--- tests/genetico_p3/config.py
+++
@@ -1,6 +0,0 @@
-k = 4
-
-class CONF:
-    fanout = k
-    weight = 'hop'
-    k_paths = k ** 2 * 3 / 4
